3dfda8ba4e0cb3d60a92b280c6c6042ad19ad594
ivan-caro <ivan.caro.leiva@gmail.com>
2024-06-09 21:49:07 -0400

    commit inicial

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const fn = () => {
     const miString = "soy un string!"
-    let b = miString + " muy feliz"
-    console.log(b)
+    let b = miString + " motivado"
+    console.log(b)    
 }
 
+// 
 fn()
 
 const clickeado = () => {
-    let a = "chanchito feliz"
+    let a = "piojo motivado"
 
     console.log(a)
 }
